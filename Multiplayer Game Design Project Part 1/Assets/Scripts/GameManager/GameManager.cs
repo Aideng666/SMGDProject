c31a4f5368dbe1fde5da0cb55ff3aa4052bc0ef4
Aiden <aiden.gimpel@ontariotechu.net>
2021-10-29 15:33:44 -0400

    Added Music Looping

--- Multiplayer Game Design Project Part 1/Assets/Scripts/GameManager/GameManager.cs
+++ Multiplayer Game Design Project Part 1/Assets/Scripts/GameManager/GameManager.cs
@@ -16,7 +16,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!FindObjectOfType<AudioManager>().IsPlaying("Level1"))
+        {
+            FindObjectOfType<AudioManager>().Play("Level1");
+        }
     }
 
     public List<GameObject> GetEnemies()
